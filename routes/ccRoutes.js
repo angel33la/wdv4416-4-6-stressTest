1182f490a762dc6317688f2cadbc3c8ae8356f73
angel33la <angel33la@outlook.com>
2026-09-25 00:42:17 -0400

    completed stress test assignment to get them to run

--- routes/ccRoutes.js
+++ routes/ccRoutes.js
@@ -1,30 +1,42 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const CreditCard = require("../models/creditCard");
 
-router.post('/', (req,res) => {
-    const { 
-        ccNumber,
-        expiration,
-        ccv
-    } = req.body
-    const creditCard = new CreditCard({
-        ccNumber,
-        expiration,
-        ccv
+router.post("/", (req, res) => {
+  const { ccNumber, expiration, ccv } = req.body || {};
+
+  const missingFields = ["ccNumber", "expiration", "ccv"].filter(
+    (field) => !req.body || !req.body[field],
+  );
+
+  if (missingFields.length > 0) {
+    return res.status(400).json({
+      message: "Missing required credit card fields",
+      fields: missingFields,
+    });
+  }
+
+  const creditCard = new CreditCard({
+    ccNumber,
+    expiration,
+    ccv,
+  });
+  console.log("Saving CreditCard");
+  creditCard
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: "CreditCard Saved",
+        data: result,
+      });
     })
-    console.log("Saving CreditCard");
-    creditCard
-        .save()
-        .then(result => {
-            res.status(201).json({
-                message: "CreditCard Saved",
-                data: result
-            })
-        })
-        .catch()
-    
-})
+    .catch((error) => {
+      console.error("Unable to save CreditCard", error);
+      res.status(500).json({
+        message: "Unable to save CreditCard",
+      });
+    });
+});
 
 module.exports = router;
